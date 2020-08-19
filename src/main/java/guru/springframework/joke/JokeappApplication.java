8af8789ff82f39858b1dc516f400805b23f61f28
A307812 <lalvarezhamann@santandertecnologia.com.ar>
2020-08-19 17:19:09 -0300

    chuck config

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -2,6 +2,7 @@ package guru.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 public class JokeappApplication {
